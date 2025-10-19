ba6421238b25443c00e49e870b34441a65df5ea6
Nguyễn Thanh Hưng <nguyenthanhhungb6@gmail.com>
2025-10-19 20:38:14 +0700

    cập nhật repo

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // !!! QUAN TRỌNG: Thay đổi 'TEN_REPO_CUA_BAN' thành tên repository trên GitHub của bạn.
-  // Ví dụ: nếu repo của bạn là https://github.com/username/my-app, bạn sẽ đặt base là '/my-app/'
-  base: '/phung-vu-vinh-son/', 
+  // Sửa lại đường dẫn base để khớp với tên repository của bạn: 'home'
+  base: '/home/', 
 })
